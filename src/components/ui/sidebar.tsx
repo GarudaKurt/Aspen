c11d4e401de196c7cb66b2cc1df7631e804a76b0
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2026-09-24 14:59:49 +0800

    Align sidebar utility import with project conventions

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "cn";
 
 type SidebarContextValue = { open: boolean; setOpen: (open: boolean) => void };
 const SidebarContext = React.createContext<SidebarContextValue | null>(null);
