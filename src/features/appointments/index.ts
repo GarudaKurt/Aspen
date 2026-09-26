a8bf7a7618a07111413bd0d948404abb44c1846e
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2026-09-26 14:48:35 +0800

    Expose reusable appointment feature surface

---
+++ src/features/appointments/index.ts
@@ -0,0 +1,8 @@
+export { AppointmentManagementPage } from "./components/appointment-management-page";
+export { AppointmentPage } from "./components/appointment-page";
+export type {
+  AppointmentDateFilter,
+  AppointmentRequest,
+  AppointmentStatus,
+  AppointmentStatusFilter,
+} from "./types";
